54d3ffabca7f40786405aa351de19b525fbd9952
Josue Soto <joshsoto6738@gmail.com>
2021-10-13 01:02:50 -0600

    Gun Functionality / Folder Orginization
    Gun stops shooting at 0 ammo and player can reload by pressing r. Gun also emits muzzle flash and impact effect. Organized Assets into folders

--- FPS Test Game/Assets/Scripts/Shooting.cs
+++ FPS Test Game/Assets/Scripts/Shooting.cs
@@ -7,48 +7,59 @@ public class Shooting : MonoBehaviour
     public float damage = 10f;
     //public float range = 100f;
     public float force = 1f;
-
+    public int magSize = 30;
+    public int ammoCount = 0;
     //references
     public ParticleSystem flash;
     public Camera fpsCam;
     public GameObject impact;
 
     //fire rate values
-    public float fireRate = 2f;
-    private float nextTimeToFire = 0f;
+    public float fireRate = 10f;
+    private float nextToFire = 0f;
+    
+    private float nextToReload = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        //if mouse-1 pressed fire
+        //if mouse1 pressed fire the shoota
         //also limits fire presses based on rate of fire values
-        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        if (Input.GetButton("Fire1") && Time.time >= nextToFire && ammoCount > 0)
         {
-            nextTimeToFire = Time.time + 1f / fireRate;
+            ammoCount--;
+            nextToFire = Time.time + 1f / fireRate;
             Shoot();
+        }
+
+        //check if user presses r and reloads mag if mag is not empty and they are not already shooting
+        if (Input.GetKeyDown(KeyCode.R) == true && Time.time >= nextToFire && ammoCount < magSize)
+        {
+
+            nextToReload = Time.time + 1f;
+            ammoCount = magSize;
         }
     }
 
     public void Shoot()
     {
+        Debug.Log(ammoCount + "/" + magSize);
         flash.Play();
         RaycastHit hit;
         //shoots ray out and returns true if ray hits an object
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
         {
-            Debug.Log(hit.transform.name);
 
             //if targeted object has rigidbody apply a force 
-            //(can add more aspects to this such as a damage handler)
+            //(can add more if statements for targeting different things such as a damage handler)
             if (hit.rigidbody)
             {
                 hit.rigidbody.AddForce(-hit.normal * force);
             }
 
-            //leave effect where ray hit object
+            //leave effect where ray hit object and delete that object 1 second later
             GameObject bullet = Instantiate(impact, hit.point, Quaternion.LookRotation(hit.normal));
-
-            Destroy(bullet);
+            Destroy(bullet, 1f);
         }
     }
 }
